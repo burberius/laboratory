b6a5127caa6c157d08de822cbed97afc6356101b
Jens Oberender <j.obi@troja.net>
2014-11-10 23:45:24 +0100

    Works with extra dropwizard-wicket project now.

--- wicket/src/main/java/net/troja/dropwizard/TestApplication.java
+++ wicket/src/main/java/net/troja/dropwizard/TestApplication.java
@@ -1,27 +1,23 @@
 package net.troja.dropwizard;
 
+import net.troja.dropwizard.wicket.WicketBundle;
+import net.troja.dropwizard.wicket.WicketConfiguration;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
-import org.apache.wicket.protocol.http.WicketFilter;
-
 public class TestApplication extends Application<WicketConfiguration> {
 
     public static void main(String[] args) throws Exception {
-        new TestApplication().run(args);
+	new TestApplication().run(args);
     }
-    
+
     @Override
     public void initialize(Bootstrap<WicketConfiguration> bootstrap) {
+	bootstrap.addBundle(new WicketBundle(WicketApplication.class));
     }
 
     @Override
     public void run(WicketConfiguration configuration, Environment environment) throws Exception {
-	WicketFilter filter = new WicketFilter(new WicketApplication());
-	filter.setFilterPath("/");
-	environment.servlets().addFilter("wicket", filter);
-	environment.jersey().disable();
     }
-
 }
--- wicket/src/main/java/net/troja/dropwizard/WicketApplication.java
+++ wicket/src/main/java/net/troja/dropwizard/WicketApplication.java
@@ -4,30 +4,30 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 
 /**
- * Application object for your web application.
- * If you want to run this application without deploying, run the Start class.
+ * Application object for your web application. If you want to run this
+ * application without deploying, run the Start class.
  * 
  * @see net.troja.dropwizard.Start#main(String[])
  */
-public class WicketApplication extends WebApplication
-{
-	/**
-	 * @see org.apache.wicket.Application#getHomePage()
-	 */
-	@Override
-	public Class<? extends WebPage> getHomePage()
-	{
-		return HomePage.class;
-	}
+public class WicketApplication extends WebApplication {
+   
+    public WicketApplication() {
+	super();
+    }
 
-	/**
-	 * @see org.apache.wicket.Application#init()
-	 */
-	@Override
-	public void init()
-	{
-		super.init();
+    /**
+     * @see org.apache.wicket.Application#getHomePage()
+     */
+    @Override
+    public Class<? extends WebPage> getHomePage() {
+	return HomePage.class;
+    }
 
-		// add your configuration here
-	}
+    /**
+     * @see org.apache.wicket.Application#init()
+     */
+    @Override
+    public void init() {
+	super.init();
+    }
 }
